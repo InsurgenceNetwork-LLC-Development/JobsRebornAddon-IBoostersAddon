80eca16bf441bf734ab6c4b3eec01e07c0b86a16
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-21 02:10:00 -0500

    multi handler update

--- src/main/java/org/insurgencedev/jobsrebornaddon/JobsRebornAddon.java
+++ src/main/java/org/insurgencedev/jobsrebornaddon/JobsRebornAddon.java
@@ -1,14 +1,17 @@
 package org.insurgencedev.jobsrebornaddon;
 
+import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 import org.insurgencedev.jobsrebornaddon.listeners.JobsEventListener;
 
-@IBoostersAddon(name = "JobsRebornAddon", version = "1.0.0", author = "InsurgenceDev", description = "JobsReborn Support")
+@IBoostersAddon(name = "JobsRebornAddon", version = "1.0.1", author = "InsurgenceDev", description = "JobsReborn Support")
 public class JobsRebornAddon extends InsurgenceBoostersAddon {
 
     @Override
     public void onAddonReloadablesStart() {
-        registerEvent(new JobsEventListener());
+        if (Bukkit.getPluginManager().isPluginEnabled("Jobs")) {
+            registerEvent(new JobsEventListener());
+        }
     }
 }
