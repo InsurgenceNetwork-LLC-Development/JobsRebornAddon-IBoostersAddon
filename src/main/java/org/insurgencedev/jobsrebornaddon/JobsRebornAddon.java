0b050f312b3a8c0f22c6a6a662f1924e1951f1ec
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-27 19:11:34 -0500

    api jar update

--- src/main/java/org/insurgencedev/jobsrebornaddon/JobsRebornAddon.java
+++ src/main/java/org/insurgencedev/jobsrebornaddon/JobsRebornAddon.java
@@ -1,16 +1,16 @@
 package org.insurgencedev.jobsrebornaddon;
 
-import org.bukkit.Bukkit;
 import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 import org.insurgencedev.jobsrebornaddon.listeners.JobsEventListener;
 
 @IBoostersAddon(name = "JobsRebornAddon", version = "1.0.1", author = "InsurgenceDev", description = "JobsReborn Support")
 public class JobsRebornAddon extends InsurgenceBoostersAddon {
 
     @Override
-    public void onAddonReloadablesStart() {
-        if (Bukkit.getPluginManager().isPluginEnabled("Jobs")) {
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("Jobs")) {
             registerEvent(new JobsEventListener());
         }
     }
--- src/main/java/org/insurgencedev/jobsrebornaddon/listeners/JobsEventListener.java
+++ src/main/java/org/insurgencedev/jobsrebornaddon/listeners/JobsEventListener.java
@@ -4,8 +4,7 @@ import com.gamingmesh.jobs.api.JobsExpGainEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
-import org.insurgencedev.insurgenceboosters.models.booster.GlobalBoosterManager;
-import org.insurgencedev.insurgenceboosters.settings.IBoostersPlayerCache;
+import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
 
 import java.util.Objects;
 
@@ -13,22 +12,23 @@ public final class JobsEventListener implements Listener {
 
     @EventHandler
     public void onGain(JobsExpGainEvent event) {
-        String type = "Jobs";
+        final String TYPE = "Jobs";
         final String NAMESPACE = "JOBS_REBORN";
-        double totalMulti = 1;
+        final double[] totalMulti = {1};
 
-        IBoostersPlayerCache.BoosterFindResult pResult = IBoosterAPI.getCache(Objects.requireNonNull(event.getPlayer().getPlayer()))
-                .findActiveBooster(type, NAMESPACE);
-        if (pResult instanceof IBoostersPlayerCache.BoosterFindResult.Success boosterResult) {
-            totalMulti += boosterResult.getBooster().getMultiplier();
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(Objects.requireNonNull(event.getPlayer().getPlayer()))
+                .getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
         }
 
-        GlobalBoosterManager.BoosterFindResult gResult = IBoosterAPI.getGlobalBoosterManager().findBooster(type, NAMESPACE);
-        if (gResult instanceof GlobalBoosterManager.BoosterFindResult.Success boosterResult) {
-            totalMulti += boosterResult.getBooster().getMultiplier();
-        }
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            totalMulti[0] += globalBooster.getMultiplier();
+            return null;
+        }, () -> null);
 
-        event.setExp(calculateAmount(event.getExp(), totalMulti));
+        event.setExp(calculateAmount(event.getExp(), totalMulti[0]));
     }
 
     private long calculateAmount(double amount, double multi) {
